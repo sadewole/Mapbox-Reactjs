50a293b0a521e83bfa0bf94235dad3ec638fbc5a
sadewole <samueladewole15@gmail.com>
2021-07-06 10:45:31 +0100

    project setup and build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Mapbox from './components/map';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
+      <Mapbox />
     </div>
   );
 }
---
+++ src/components/map/index.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useRef, useCallback } from 'react';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+import mbxGeocoding from '@mapbox/mapbox-sdk/services/geocoding';
+
+import './map.css';
+
+const GenerateMap = () => {
+  const mapContainerRef = useRef(null);
+  mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API;
+
+  const fetchData = useCallback(() => {
+    const geocodingClient = mbxGeocoding({
+      accessToken: mapboxgl.accessToken,
+    });
+
+    // geocoding with countries
+    return geocodingClient
+      .forwardGeocode({
+        query: 'Ikeja, Lagos',
+        countries: ['ng'],
+        limit: 2,
+      })
+      .send()
+      .then((response) => {
+        const match = response.body;
+        const coordinates = match.features[0].geometry.coordinates;
+        const placeName = match.features[0].place_name;
+
+        return {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: coordinates,
+          },
+          properties: {
+            description: placeName,
+          },
+        };
+      });
+  }, []);
+
+  useEffect(() => {
+    const results = fetchData();
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      zoom: 9,
+      center: [3.361881, 6.672557],
+    });
+
+    results.then((marker) => {
+      // create a HTML element for each feature
+      var el = document.createElement('div');
+      el.className = 'circle';
+
+      // make a marker for each feature and add it to the map
+      new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .setPopup(
+          new mapboxgl.Popup({ offset: 25 }) // add popups
+            .setHTML('<p>' + marker.properties.description + '</p>')
+        )
+        .addTo(map);
+    });
+
+    // clean up on unmount
+    return () => map.remove();
+  }, [fetchData]);
+
+  return (
+    <div>
+      <div
+        ref={mapContainerRef}
+        style={{ width: '100%', height: '500px', borderRadius: '5px' }}
+      />
+    </div>
+  );
+};
+
+export default GenerateMap;
